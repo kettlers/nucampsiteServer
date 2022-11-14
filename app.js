32eaa27aa2fb63ab7d6a2dded0020aa55dda53c3
Sung Eun Kettler <kettlers@oldwestbury.edu>
2022-11-14 12:52:29 -0500

    basic authentication

--- app.js
+++ app.js
@@ -34,6 +34,33 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//we place auth before we use the express.static middleware because otherwise users
+//will have no restrictions accessing static data without authenticating
+function auth(req, res, next) {
+  console.log(req.headers)
+  const authHeader = req.headers.authorization
+  if (!authHeader) {
+    const err = new Error('You are not authenticated!')
+    res.setHeader('WWW-Authenticate', 'Basic')
+    err.status = 401
+    return next(err)
+  }
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
+  const user = auth[0]
+  const pass = auth[1]
+  if (user === 'admin' && pass === 'password') {
+    return next()
+  } else {
+    const err = new Error('You are not authenticated!')
+    res.setHeader('WWW-Authenticate', 'Basic')
+    err.status = 401
+    return next(err)
+  }
+}
+
+app.use(auth)
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
