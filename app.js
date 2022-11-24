2830e9960b7c8a67ed6023826dfcc1bdbd5483b2
Sung Eun Kettler <kettlers@oldwestbury.edu>
2022-11-24 12:24:28 -0500

    file upload

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 const passport = require('passport')
 const config = require('./config')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter')
 
 const mongoose = require('mongoose')
 
@@ -26,6 +27,15 @@ connect.then(() => console.log('Connected correctly to server'),
 )
 
 var app = express();
+
+app.all('*', (req,res,next)=>{
+  if(req.secure){
+    return next()
+  } else {
+    console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`)
+    res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`)
+  }
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,6 +55,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
